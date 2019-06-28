c5a931913a93a04bc6aebaf88873e32ea2edc24e
Kevin Gut <kevin.gut@kauz.ch>
2019-06-28 08:48:47 +0200

    Cleanup and test code

--- TOTP/Program.cs
+++ TOTP/Program.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 using System.Linq;
-using System.Security.Cryptography;
 
 namespace TOTP
 {
     class Program
     {
-        //public const string CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-
         static void Main(string[] args)
         {
             var KEY = "12345678901234567890".Select(m => (byte)m).ToArray();
             Console.Error.WriteLine(Base32.ToBase32String(KEY));
-            /*
+            //*
             do
             {
                 Console.Clear();
                 Console.Error.WriteLine("TOTP Generated:\t{0}", DateTime.Now.ToLongTimeString());
-                var CurrentOffset = GetCounter();
-                Console.Error.WriteLine("TOTP Counter:\t{0}", CurrentOffset);
-                Console.Error.WriteLine("TOTP Token:\t{0:000000}", TOTP(KEY, CurrentOffset));
+                var CurrentOffset = HOTP.GetCounter() - 5;
+                for (var i = 0; i < 5; i++)
+                {
+                    Console.Error.WriteLine("TOTP Counter:\t{0}", CurrentOffset + i);
+                    Console.Error.WriteLine("TOTP Token:\t{0:000000}", HOTP.GetHOTP(KEY, CurrentOffset + i));
+                }
             }
             while (Console.ReadKey(true).Key != ConsoleKey.Escape);
             //*/
